e36affd3708a49a5a94937419bcb6cc9a421af63
Naz1982 <nazarenoloiacono@gmail.com>
2024-10-23 20:14:14 +0200

    Add files via upload
    fixed route server

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // Add this at the top of your App.js
 const apiUrl = process.env.NODE_ENV === 'production'
-    ? 'https://desdemona.onrender.com/api'  // Replace with your actual Render backend URL
+    ? 'https://dss-desdemona.onrender.com/api'  // Replace with your actual Render backend URL
     : '/api';  // This will work with the proxy during development
 
 export default apiUrl;
